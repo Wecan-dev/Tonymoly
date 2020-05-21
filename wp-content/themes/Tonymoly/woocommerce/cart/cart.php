fe1a2a55625a2661e3f7c74900e69b021c90c89a
dannyunico <danielbermudezreyes@gmail.com>
2020-05-21 11:29:50 -0400

    changes cart

--- wp-content/themes/Tonymoly/woocommerce/cart/cart.php
+++ wp-content/themes/Tonymoly/woocommerce/cart/cart.php
@@ -34,7 +34,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 					?>
 
 				
-					<div class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+					<div class="container mb-4 woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 						
 
 						<div class="product-cart">
@@ -135,9 +135,19 @@ do_action( 'woocommerce_before_cart' ); ?>
 </div>	
 </form>
 
+<div class="oferta-cart mt-5">
+	<p>Compra 100 mil y obtén tu envio gratis</p>
+</div>
+
 <?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
 
-<div class="cart-collaterals">
+<div class="cart-collaterals container pt-4">
+<?php if ( wc_coupons_enabled() ) { ?>
+						<div class="coupon cupon-cart">
+							<label class="d-none" for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Ingresa un cupon o código de afiliado', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Aplicar', 'woocommerce' ); ?>"><?php esc_attr_e( 'Aplicar', 'woocommerce' ); ?></button>
+							<?php do_action( 'woocommerce_cart_coupon' ); ?>
+						</div>
+					<?php } ?>
 	<?php
 		/**
 		 * Cart collaterals hook.
@@ -155,12 +165,6 @@ do_action( 'woocommerce_before_cart' ); ?>
 			<tr>
 				<td colspan="6" class="actions">
 
-					<?php if ( wc_coupons_enabled() ) { ?>
-						<div class="coupon">
-							<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
-							<?php do_action( 'woocommerce_cart_coupon' ); ?>
-						</div>
-					<?php } ?>
 
 					<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
 
