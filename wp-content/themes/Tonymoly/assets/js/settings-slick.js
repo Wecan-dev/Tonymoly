c23092e13c4d7f9d98dc581e5942a370c998bfb4
Gladibeth <gladibethdelosa@gmail.com>
2020-05-22 10:04:27 -0400

    Add changes styles

--- wp-content/themes/Tonymoly/assets/js/settings-slick.js
+++ wp-content/themes/Tonymoly/assets/js/settings-slick.js
@@ -4,47 +4,25 @@ $('.main-categories__content').slick({
   centerMode: true,
   speed: 500,
   cssEase: 'linear',
-  centerPadding: '60px',
+  centerPadding: '25px',
   slidesToScroll: 1,
-  arrows: false,
-  responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerPadding: '0',
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        centerPadding: '0px',
-        centerMode:true,
+  arrows: false
 
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        infinite: true,
-        slidesToShow: 1,
-        centerMode: true,
-        speed: 500,
-        cssEase: 'linear',
-        centerPadding: '105px',
-        slidesToScroll: 1,
-        arrows: false
-      }
-    }
-  ]
+});
+
+$('.main-product__content').slick({
+  infinite: true,
+  slidesToShow: 2,
+  speed: 500,
+  cssEase: 'linear',
+  slidesToScroll: 1,
+  arrows: false
+
 });
 
 
-$('.main-product__content').slick({
+
+$('').slick({
   infinite: true,
   autoplay: true,
   slidesToShow: 2,
@@ -52,34 +30,7 @@ $('.main-product__content').slick({
   dots: false,
   arrows:false,
   fade: true,
-  cssEase: 'linear',
-  responsive: [{
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 5000
-      }
-    }
-  ]
+  cssEase: 'linear'
 });
 
 
@@ -93,79 +44,15 @@ $('.main-categoriesAll__content').slick({
   arrows:false,
   fade: true,
   cssEase: 'linear',
-  responsive: [{
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 5000
-      }
-    }
-  ]
 });
 
 $('.main-tv__content').slick({
   infinite: true,
-  slidesToShow: 2,
+  slidesToShow: 1,
   centerMode: true,
   speed: 500,
   cssEase: 'linear',
-  centerPadding: '60px',
+  leftPadding: '50px',
   slidesToScroll: 1,
-  arrows: false,
-  responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerPadding: '0',
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        centerPadding: '0px',
-        centerMode:true,
-
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        infinite: true,
-        slidesToShow: 1,
-        centerMode: true,
-        speed: 500,
-        cssEase: 'linear',
-        centerPadding: '25px',
-        slidesToScroll: 1,
-        arrows: false
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+  arrows: false
 });
--- wp-content/themes/Tonymoly/home.php
+++ wp-content/themes/Tonymoly/home.php
@@ -43,8 +43,8 @@
 </section>
 
 <section class="main-categories">
-  <div class="container">
-    <div class="main-title__between">
+  <div class="">
+    <div class="main-title__between container">
       <div class="main-title__general">
         <p>Categorias populares</p>
       </div>
@@ -117,6 +117,31 @@
       </div>
     </div>
     <div class="main-product__content">
+      <div class="main-product__item">
+        <div class="main-product__card">
+          <div class="main-product__img">
+            <img src="<?php echo get_template_directory_uri();?>/assets/img/Product/1.jpg" alt="">
+          </div>
+          <div class="main-product__bag">
+            <img src="<?php echo get_template_directory_uri();?>/assets/img/bag-black.svg" alt="">
+
+          </div>
+          <div class="main-product__like">
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+          </div>
+          <div class="main-product__box">
+            <div class="main-title__categories title-black">
+              <p>CREMAS</p>
+            </div>
+            <div class="main-product__description">
+              <p>Crema de te verde para iluminar el rostro   </p>
+            </div>
+            <div class="main-title__categories">
+              <p>$12.22</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="main-product__item">
         <div class="main-product__card">
           <div class="main-product__img">
@@ -391,8 +416,8 @@
 
 
 <section class="main-tv">
-  <div class="container">
-    <div class="main-title__general main-title__general--center">
+  <div class="">
+    <div class="container main-title__general main-title__general--center">
       <p>TONY. <span>TV</span></p>
     </div>
     <div class="main-tv__content">
