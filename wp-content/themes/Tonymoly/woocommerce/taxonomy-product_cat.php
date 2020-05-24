b97ca8f9441e9f6d2c9447b97417d07ae83bdb77
Yonnys Quintero <leafar.qr@gmail.com>
2020-05-24 16:35:51 -0400

    woocommerce

--- wp-content/themes/Tonymoly/woocommerce/taxonomy-product_cat.php
+++ wp-content/themes/Tonymoly/woocommerce/taxonomy-product_cat.php
@@ -18,5 +18,51 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+?>
+
+<?php get_header(); ?>
+<section class="main-productfull">
+	<div class="container container__position">
+		<div class="main-productfull__content">
+			<div class="main-productfull__item">
+				<div class="main-productfull__img">
+					<img src="<?php echo get_template_directory_uri();?>/assets/img/ProductFull/1.jpg" alt="">
+				</div>
+				<div class="main-productFull__btn">
+					<a class="btn_custom btn--small btn--filled arrowsline" href="#">
+						Comprar      
+					</a>
+				</div>
+			</div>
+			<div class="main-productfull__item">
+				<div class="main-productfull__img">
+					<img src="<?php echo get_template_directory_uri();?>/assets/img/ProductFull/1.jpg" alt="">
+				</div>
+				<div class="main-productFull__btn">
+					<a class="btn_custom btn--small btn--filled arrowsline" href="#">
+						Comprar      
+					</a>
+				</div>
+			</div>
+			<div class="main-productfull__item">
+				<div class="main-productfull__img">
+					<img src="<?php echo get_template_directory_uri();?>/assets/img/ProductFull/1.jpg" alt="">
+				</div>
+				<div class="main-productFull__btn">
+					<a class="btn_custom btn--small btn--filled arrowsline" href="#">
+						Comprar      
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+			<span class="slider__label sr-only">
+			</div>
+		</span>
+	</div>
+</section>
+<?php
 
 wc_get_template( 'archive-product.php' );
+get_footer(); ?>
